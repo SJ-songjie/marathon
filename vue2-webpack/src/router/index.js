89f9cb4cfd6c76a5e8c08f3d7e681227d97274ae
songjie <2074485243@qq.com>
2023-12-12 20:26:16 +0800

    modified:   vue2-webpack/package.json 	deleted:    vue2-webpack/src/common/loginPage.vue 	deleted:    vue2-webpack/src/common/mainPage.vue 	modified:   vue2-webpack/src/main.js 	new file:   vue2-webpack/src/pages/login/index.vue 	renamed:    vue2-webpack/src/assets/images/bg.jpg -> vue2-webpack/src/public/assets/images/bg.jpg 	modified:   vue2-webpack/src/router/index.js

--- vue2-webpack/src/router/index.js
+++ vue2-webpack/src/router/index.js
@@ -3,27 +3,17 @@ import Router from "vue-router";
 
 Vue.use(Router);
 
-let routes = [];
-// 登录
-const loginRoute = {
-  path: "/login",
-  name: "Login",
-  component: () => import("../common/loginPage"),
-};
-
-// 首页
-const mainRoute = {
-  path: "/main",
-  name: "Main",
-  component: () => import("../common/mainPage"),
-};
-
-routes.push(loginRoute, mainRoute);
-
-routes.unshift({
-  path: "*",
-  redirect: routes[0].path,
-});
+let routes = [
+  {
+    path: "*",
+    redirect: "/login",
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../pages/login/index.vue"),
+  },
+];
 
 const router = new Router({
   routes,
